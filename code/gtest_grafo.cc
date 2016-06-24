048707146483a8e16ceba5c6b6eda90c7533faa9
Victri <victoria@ubuntu.(none)>
2016-06-24 15:36:08 -0300

    Teste para função de editar tarefas, recuperar tarefas e pre-requisitos por ID

--- code/gtest_grafo.cc
+++ code/gtest_grafo.cc
@@ -102,6 +102,90 @@ TEST(TestRemovePreRequisito,remove_prerequisitos) {
 
 }
 
+TEST(TestPesquisaPreRequisito,pesquisa_prerequisitos) {
+    char str1[] = "primeira";
+    char str2[] = "segunda";
+    char str3[] = "terceira";
+    char str4[] = "quarta";
+    int T1, T2;
+
+    grafo* G = cria_grafo();
+    ASSERT_TRUE(G);
+
+    G = insere_tarefa(G, 1, str1, 0, 1, 0, 0);
+    G = insere_tarefa(G, 2, str2, 0, 2, 2, 0);
+    G = insere_tarefa(G, 3, str3, 0, 3, 5, 0);
+    G = insere_tarefa(G, 4, str4, 5, 2, 5, 3);
+
+    G = insere_prerequisitos(G, 4, 1);
+    G = insere_prerequisitos(G, 4, 2);
+    G = insere_prerequisitos(G, 4, 3);
+    
+    T1 = pesquisa_prerequisitos(G, 4, 3);
+    T2 = pesquisa_prerequisitos(G, 1, 2);
+
+    EXPECT_EQ(T1, 1);
+    EXPECT_EQ(T2, 0);
+
+}
+
+TEST(TestEditaTarefa,edita_tarefa) {
+    char str1[] = "primeira";
+    char str2[] = "segunda";
+    char str3[] = "terceira";
+    char str4[] = "quarta";
+    char nome_tarefa[] = "nome alterado";
+    int id_prerequisitos[10];
+
+    grafo* G = cria_grafo();
+    ASSERT_TRUE(G);
+
+    G = insere_tarefa(G, 1, str1, 0, 1, 0, 0);
+    G = insere_tarefa(G, 2, str2, 0, 2, 2, 0);
+    G = insere_tarefa(G, 3, str3, 0, 3, 5, 0);
+    G = insere_tarefa(G, 4, str4, 5, 2, 5, 3);
+
+    G = insere_prerequisitos(G, 4, 1);
+    G = insere_prerequisitos(G, 4, 2);
+    G = insere_prerequisitos(G, 4, 3);
+
+    id_prerequisitos[0] = 2;
+
+    G = edita_tarefa(G, 4, 5, nome_tarefa, 0, 3, 6, 1, id_prerequisitos, FLG_NOME|FLG_INIC|FLG_PRER|FLG_EXEC|FLG_DURA|FLG_IDTR);    
+
+    EXPECT_EQ(G->T->prox->prox->prox->id_tarefa, 5);
+}
+
+
+
+TEST(TestRecuperarTarefaPorID, procura_tarefa) {
+
+    char str1[] = "primeira";
+    char str2[] = "segunda";
+    char str3[] = "terceira";
+    char str4[] = "quarta";
+    int id_prerequisitos[10];
+    tarefa * T;
+
+    grafo* G = cria_grafo();
+    ASSERT_TRUE(G);
+
+    G = insere_tarefa(G, 1, str1, 0, 1, 0, 0);
+    G = insere_tarefa(G, 2, str2, 0, 2, 2, 0);
+    G = insere_tarefa(G, 3, str3, 0, 3, 5, 0);
+    G = insere_tarefa(G, 4, str4, 5, 2, 5, 3);
+
+    G = insere_prerequisitos(G, 2, 1);
+    G = insere_prerequisitos(G, 3, 2);
+    G = insere_prerequisitos(G, 4, 3);
+
+    T = procura_tarefa(G, 7);
+
+    ASSERT_TRUE(T == NULL);
+    ASSERT_EQ(pesquisa_tarefa(G,3), 1);
+
+}
+
 int main(int argc, char **argv) {
         ::testing::InitGoogleTest( &argc, argv );
         return RUN_ALL_TESTS();
